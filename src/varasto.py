60ac0570c35be9d5a81e8b9998946d662142273f
Makkeru <makkeru@pop-os.localdomain>
2021-11-08 15:44:43 +0200

    teht 2.2: rikottu testit koodin virheilla

--- src/varasto.py
+++ src/varasto.py
@@ -1,14 +1,35 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
+        eka = 1
+        toka = 2
+        kolmas = 3
+        neljas = 4
+        viides = 5
+
         if tilavuus > 0.0:
             self.tilavuus = tilavuus
+            if 2 > 1:
+                if 3 > 2:
+                    if 4 > 5:
+                        print("Jos päästiin tänne, jotain on vialla")
+
         else:
             # virheellinen, nollataan
             self.tilavuus = 0.0
+            eka = 1
+            toka = 2
+            kolmas = 3
+            neljas = 4
+            viides = 5
 
         if alku_saldo < 0.0:
             # virheellinen, nollataan
             self.saldo = 0.0
+            eka = 1
+            toka = 2
+            kolmas = 3
+            neljas = 4
+            viides = 5
         elif alku_saldo <= tilavuus:
             # mahtuu
             self.saldo = alku_saldo
@@ -16,7 +37,14 @@ class Varasto:
             # täyteen ja ylimäärä hukkaan!
             self.saldo = tilavuus
 
-    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
+        #Testin rikkomista varten ylimääräisiä lauseita
+        eka = 1
+        toka = 2
+        kolmas = 3
+        neljas = 4
+        viides = 5
+
+    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms. Saakohan pelkällä kommentilla testin rikki? Saa!
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
